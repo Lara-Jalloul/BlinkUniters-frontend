5aece1826d38553cc06a5999a3991eb6be90f448
Lara-Jalloul <laraadjalloul@gmail.com>
2021-07-06 21:00:38 +0300

    create Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,40 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import Bands from "./components/Bands";
+import News from "./components/News";
+import Faq from "./components/Faq";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Router>
+        <Navbar />
+
+        <Switch>
+          <Route path="/" component={Home} exact>
+            <Home />
+          </Route>
+          <Route path="/bands" component={Bands} exact>
+            <Bands />
+          </Route>
+          <Route path="/news" component={News} exact>
+            <News />
+          </Route>
+          <Route path="/faq" component={Faq} exact>
+            <Faq />
+          </Route>
+          <Route path="/aboutus" component={About} exact>
+            <About />
+          </Route>
+          <Route path="/contactus" component={Contact} exact>
+            <Contact />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+function Navbar() {
+  const [isMobile, setIsMobile] = useState(false);
+  return (
+    <nav className="navbar">
+      <h3 className="logo">BlinkUniters</h3>
+      <ul
+        className={isMobile ? "nav-links-mobile" : "nav-links"}
+        onClick={() => setIsMobile(false)}
+      >
+        <Link to="/" className="home">
+          <li>Home</li>
+        </Link>
+        <Link to="/bands" className="bands">
+          <li>Bands</li>
+        </Link>
+        <Link to="/news" className="news">
+          <li>News</li>
+        </Link>
+        <Link to="/faq" className="faq">
+          <li>FAQ</li>
+        </Link>
+        <Link to="/aboutus" className="about">
+          <li>About Us</li>
+        </Link>
+        <Link to="/contactus" className="contact">
+          <li>Contact Us</li>
+        </Link>
+      </ul>
+      <button
+        className="mobile-menu-icon"
+        onClick={() => setIsMobile(!isMobile)}
+      >
+        {isMobile ? (
+          <i className="fas fa-times"></i>
+        ) : (
+          <i className="fas fa-bars"></i>
+        )}
+      </button>
+    </nav>
+  );
+}
+
+export default Navbar;
