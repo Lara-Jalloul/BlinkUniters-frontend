f84550d92cbbe750ca3370043a5e4feae7ece451
bassamhamdar <hamdar.m.bassam@gmail.com>
2021-07-09 17:59:07 +0300

    home footer added to home

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,8 +1,13 @@
 import React from "react";
-
+import Footer from "./HomeFooter/Footer";
 
 function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      Home Page
+      <Footer />
+    </div>
+  );
 }
 
 export default Home;
