5761d4febb909ee18609222cacf07abd73d02e4d
bassamhamdar <hamdar.m.bassam@gmail.com>
2021-07-09 20:41:52 +0300

    social madia links added to footer

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
-
+import Footer from "./HomeFooter/Footer";
 function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      Home Page Slider Cards
+      <Footer />
+    </div>
+  );
 }
 
 export default Home;
--- frontend/src/components/HomeFooter/Footer.js
+++ frontend/src/components/HomeFooter/Footer.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { FaInstagram, FaFacebook, FaTwitter } from "react-icons/fa";
+import "./Footer.css";
 export default function Footer() {
   return (
-    <div className="Home_Footer">
-      <a href="https://www.instagram.com/">
-        <FaInstagram />
-      </a>
-      <a href="https://www.facebook.com/">
-        <FaFacebook />
-      </a>
-      <a href="https://www.twitter.com/">
-        <FaTwitter />
-      </a>
-    </div>
+    <>
+      <hr className="Footer_hrLine" />
+      <div className="Home_Footer">
+        <a href="https://www.instagram.com/">
+          <FaInstagram className="React_Icon" />
+        </a>
+        <a href="https://www.facebook.com/">
+          <FaFacebook className="React_Icon" />
+        </a>
+        <a href="https://www.twitter.com/">
+          <FaTwitter className="React_Icon" />
+        </a>
+      </div>
+    </>
   );
 }
