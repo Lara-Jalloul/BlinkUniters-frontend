36db876500582dfa06176034aa99fe7bf8a45515
bassamhamdar <hamdar.m.bassam@gmail.com>
2021-07-11 12:15:25 +0300

    home Slider has been added

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Footer from "./HomeFooter/Footer";
+import HomeSlider from "./HomeSlider/HomeSlider";
 function Home() {
   return (
     <div>
       Home Page Slider Cards
+      <HomeSlider />
       <Footer />
     </div>
   );
