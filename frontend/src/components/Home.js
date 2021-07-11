79722e9b1ed537372f2d929283f3c4930dd1f37f
bassamhamdar <hamdar.m.bassam@gmail.com>
2021-07-11 17:27:36 +0300

    made some css changes for home page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,10 +3,10 @@ import Footer from "./HomeFooter/Footer";
 import HomeSlider from "./HomeSlider/HomeSlider";
 import HomeCards from "./HomeCards/HomeCards";
 import HomeTestimonial from "./HomeTestimonials/HomeTestimonial";
+import "./Home.css";
 function Home() {
   return (
-    <div>
-      Home Page Slider Cards
+    <div className="Home_page">
       <HomeSlider />
       <HomeCards />
       <HomeTestimonial />
