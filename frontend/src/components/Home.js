db320e90c4739e21a7349acbccde9a4a86e35e66
bassamhamdar <hamdar.m.bassam@gmail.com>
2021-07-12 11:31:00 +0300

    fix some problems

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,21 +5,6 @@ import HomeCards from "./HomeCards/HomeCards";
 import HomeTestimonial from "./HomeTestimonials/HomeTestimonial";
 import "./Home.css";
 function Home() {
-  const [data, setData] = useState([{ writer_name: "", Comment: "" }]);
-
-  useEffect(() => {
-    fetch("/")
-      .then((res) => {
-        if (res.ok) {
-          console.log(res);
-          return res.json();
-        }
-      })
-      .then((jsonRes) => {
-        setData(jsonRes);
-      });
-  }, []);
-
   return (
     <div className="Home_page ">
       <HomeSlider />
--- frontend/src/components/HomeTestimonials/HomeTestimonial.js
+++ frontend/src/components/HomeTestimonials/HomeTestimonial.js
@@ -1,43 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
 import "./HomeTestimonial.css";
 import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
 
-export default function HomeTestimonial(props) {
-  console.log(props);
+export default function HomeTestimonial() {
+  const [data, setData] = useState([{ writer_name: "", Comment: "" }]);
+
+  useEffect(() => {
+    fetch("/testimonials")
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then((jsonRes) => {
+        console.log("json", jsonRes);
+        setData(jsonRes);
+      });
+  }, []);
+
   return (
-    <div className="testimonial_box">
-      <AliceCarousel responsive={"responsive"} animationType="fadeout">
-        <div className="Home_testimonial">
-          <h3>BASSAM HAMDAR</h3>
-          <FaQuoteLeft className="quotation-left" />
-
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-            perferendis doloremque culpa quas deserunt. Tempora maxime
-            voluptates nisi harum aperiam nostrum in enim necessitatibus
-            accusamus, dicta fugit ex. Maiores unde exercitationem nihil
-            blanditiis a delectus nemo deleniti odio magnam suscipit
-            dignissimos, assumenda qui at dolore, officia accusamus, perferendis
-            sequi. Delectus!
-          </p>
-          <FaQuoteRight className="quotation-right" />
-        </div>
-        <div className="Home_testimonial">
-          <h3>JOSEPH HARMANDRIAN</h3>
-          <FaQuoteLeft className="quotation-left" />
-
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Praesentium aliquid ex numquam doloribus fugit sint? Molestiae
-            nesciunt aliquid, ipsa ducimus sapiente quibusdam omnis temporibus
-            vitae non. Tenetur dolore sed, nostrum deserunt a architecto non
-            rerum quia sapiente porro neque iure!
-          </p>
-          <FaQuoteRight className="quotation-right" />
-        </div>
-      </AliceCarousel>
-    </div>
+    <>
+      <div className="testimonial_box">
+        <AliceCarousel responsive={"responsive"} animationType="fadeout">
+          {data.map((data, index) => (
+            <div className="Home_testimonial" key={index}>
+              <h3>{data.writer_name}</h3>
+              <FaQuoteLeft className="quotation-left" />
+              <p>{data.comment}</p>
+              <FaQuoteRight className="quotation-right" />
+            </div>
+          ))}
+        </AliceCarousel>
+      </div>
+      ;
+    </>
   );
 }
